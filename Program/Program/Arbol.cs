05a914ced778965774d11bed41ae648cd6ccc97e
Walter Meléndez <melendez@gmail.com>
2024-12-19 14:01:48 -0600

    Inicio de los arboles en C#

---
+++ Program/Program/Arbol.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Arbol
+    {
+        private CNodo raiz;
+        private CNodo trabajo;
+        private int i = 0;
+        public Arbol() { 
+        raiz = new CNodo();
+
+        }
+        public CNodo Insertar(string Pdato, CNodo pNodo)
+        {
+            if (pNodo==null)//Si esta vacio
+            {
+                raiz= new CNodo();
+                raiz.Dato = Pdato;
+                raiz.Hijo = null;
+                raiz.Hermano = null;
+                return raiz;
+            }
+            if (pNodo.Hijo==null) // Se inserta como nuevo hijo.
+            {
+                CNodo temp = new CNodo();
+                temp.Dato = Pdato;
+                pNodo= temp;
+                return temp;
+
+            }
+            else //Caso cuando el nodo es un hermando del hijo
+            {
+                trabajo = pNodo.Hijo;
+
+                while (trabajo.Hermano!=null)
+                {
+                        trabajo= trabajo.Hermano;
+                }
+                CNodo temp = new CNodo();
+                temp.Dato = Pdato;  
+                trabajo.Hermano=temp;
+                return temp;
+            }
+        }
+		
+    }
+
+}
---
+++ Program/Program/CNodo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class CNodo
+    {
+        private string dato;
+        private CNodo hijo;
+        private CNodo hermano;
+        public string Dato { get => dato; set => dato = value; }
+        public CNodo Hijo { get => hijo; set => hijo = value; }
+        public CNodo Hermano { get => hermano; set => hermano = value; }
+
+        public CNodo() {
+
+            dato = "";
+            hijo = null;
+            hermano = null;
+        }
+    }
+
+
+}
+
